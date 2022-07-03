ce324468bf081ccde0cbd7ef709764dc0cc0bf93
RnZ@beetswars <RnZ@gvs.dyn.bawue.de>
2022-07-03 21:15:11 +0200

    add spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Spinner() {
+  return (
+    <div className="spinner-wrapper">
+      <div className="loading-spinner"></div>
+    </div>
+  )
+}
