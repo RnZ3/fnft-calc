9e6cb53e047c22432e3f775337caaf00b4b9438e
RnZ@beetswars <RnZ@gvs.dyn.bawue.de>
2022-07-31 16:48:05 +0200

    wip ps offers

--- src/components/PaintSwap.tsx
+++ src/components/PaintSwap.tsx
@@ -5,26 +5,26 @@ import { useGlobalContext } from "context/context";
 const psUrl = "https://paintswap.finance/marketplace/";
 const psSales =
   "https://api.paintswap.finance/v2/sales?onlyActive=true&sold=false&collections[0]=0x58a57754e8d8693703e51604696bd065f25333fd";
+var salesData: any[] = [];
 
 export function PaintSwap(props: any) {
   const { fnftId, setCopy } = useGlobalContext();
-  //const [metaLoaded, setMetaLoaded] = useState(false);
-  //const [meta, setMeta] = useState([]);
+  const [error, setError] = useState(null);
+  const [metaLoaded, setMetaLoaded] = useState(false);
+  const [meta, setMeta] = useState([]);
   const [checkPs, setCheckPs] = useState(false);
   const [saleLoaded, setSaleLoaded] = useState(false);
   const [onsale, setOnsale] = useState([]);
   let salesArr: [] = [];
 
-/*
-  const fetchMeta = async () => {
-    const res = await fetch(apiRevestUrl, { redirect: 'follow' } )
+  const fuBar = async (apiRevestUrl: string) => {
+    const res = await fetch(apiRevestUrl, { redirect: "follow" });
     res
       .json()
-      .then(res => setMeta(res))
-      .catch(err => setError(err))
-      setMetaLoaded(true)
-  }
-*/
+      .then((res) => setMeta(res))
+      .catch((err) => setError(err));
+    //      setMetaLoaded(true)
+  };
 
   const fetchPs = async () => {
     const res = await fetch(psSales);
@@ -38,53 +38,82 @@ export function PaintSwap(props: any) {
     setCopy(e);
   }
 
+  const fetchMeta = async () => {
+    await Promise.all(
+      (salesArr = fnftOnsale.sales.map(async (ons: any) => {
+        const apiRevestUrl =
+          "https://api.revest.finance/metadata?id=" +
+          ons.tokenId +
+          "&chainId=250";
+
+        await fetch(apiRevestUrl || "")
+          .then((response) => {
+            return response.json();
+          })
+          .then((response) => {
+            const data: any = {
+              id: ons.id,
+              tokenid: ons.tokenId,
+              price: ons.price / 1000000000000000000,
+              endtime: ons.endTime,
+              isauction: ons.isAuction,
+              asset: response.properties.asset_name,
+            };
+            salesData.push(data);
+            console.log("inside map");
+            return data;
+          });
+      }))
+    );
+    setMetaLoaded(true);
+  };
+
   useEffect(() => {
     //setMetaLoaded(false);
     //setSaleLoaded(false);
     if (checkPs && !saleLoaded) {
       fetchPs();
     }
+    if (checkPs && saleLoaded) {
+      fetchMeta();
+    }
     //      fetchMeta();
-  }, [checkPs]);
+  }, [checkPs, saleLoaded]);
 
-  console.log(onsale);
+  console.log(checkPs, saleLoaded, metaLoaded);
 
-  if (checkPs && saleLoaded && onsale) {
-    const fnftOnsale = JSON.parse(JSON.stringify(onsale));
-    salesArr = fnftOnsale.sales.map((ons: any) => {
-      return {
-        id: ons.id,
-        tokenid: ons.tokenId,
-        price: ons.price / 1000000000000000000,
-        endtime: ons.endTime,
-        isauction: ons.isAuction,
-      };
-    });
+  const fnftOnsale = JSON.parse(JSON.stringify(onsale));
 
+  //console.log(salesArr)
+  console.log(salesData);
+
+  if (checkPs && saleLoaded && metaLoaded) {
     return (
       <>
         <div>
           <hr />
           <p>
-            currently <b>{salesArr.length}</b> fNFT offered on PaintSwap:
+            currently <b>{salesData.length}</b> fNFT offered on PaintSwap:
           </p>
           <table>
             <tbody>
               <tr className="tb">
                 <td>fNFT id</td>
+                <td>Asset name</td>
                 <td>Price</td>
-                <td>auction?</td>
+                <td>Auction?</td>
                 <td>End time</td>
                 <td>PS link</td>
               </tr>
-              {salesArr.map((ps: any, i: number) => (
+              {salesData.map((ps: any, i: number) => (
                 <tr key={i}>
                   <td>
                     <button onClick={() => handlePs(ps.tokenid)}>
                       {ps.tokenid}
                     </button>
                   </td>
-                  <td>({ps.price} FTM)</td>
+                  <td>{ps.asset}</td>
+                  <td>{ps.price} FTM</td>
                   <td>{ps.isauction ? "auction" : "sale"}</td>
                   <td>{new Date(ps.endtime * 1000).toUTCString()}</td>
                   <td>
@@ -99,16 +128,16 @@ export function PaintSwap(props: any) {
         </div>
       </>
     );
-  } else if ( checkPs && saleLoaded ) {
-    return <p>currently no offers on PaintSwap</p>;
   } else {
     return (
       <>
         <div>
           <hr />
-      <p>
-        <button onClick={() => setCheckPs(true)}>check PaintSwap offers</button>
-      </p>
+          <p>
+            <button onClick={() => setCheckPs(true)}>
+              check PaintSwap offers
+            </button>
+          </p>
         </div>
       </>
     );
